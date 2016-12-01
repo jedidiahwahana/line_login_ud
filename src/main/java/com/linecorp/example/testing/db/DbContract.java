4d1bcccedbe485dc439d1b25aa983ade1168742e
LINE <line@LINE_ID_8cb6d3.local>
2016-12-01 14:56:52 +0700

    fix: Change host and db name

--- src/main/java/com/linecorp/example/testing/db/DbContract.java
+++ src/main/java/com/linecorp/example/testing/db/DbContract.java
@@ -1,8 +1,8 @@
 package com.linecorp.example.testing.db;
 
 public interface DbContract {
-    public static final String HOST = "jdbc:postgresql://localhost:5432/";
-    public static final String DB_NAME = "test1";
+    public static final String HOST = "jdbc:postgres://bnujrtlweyppfu:_sbmES7FJoG1X4RISbcetjQX8K@ec2-54-235-65-139.compute-1.amazonaws.com:5432/db6co2hp5r1sdg";
+    public static final String DB_NAME = "frozendb";
     public static final String USERNAME = "line";
     public static final String PASSWORD = "line";
 }
