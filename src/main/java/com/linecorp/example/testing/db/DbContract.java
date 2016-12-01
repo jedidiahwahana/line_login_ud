b601b620d17ae7e227c44cd51274652eda2ff77c
LINE <line@LINE_ID_8cb6d3.local>
2016-12-01 15:28:29 +0700

    fix: Change username and password for database

--- src/main/java/com/linecorp/example/testing/db/DbContract.java
+++ src/main/java/com/linecorp/example/testing/db/DbContract.java
@@ -3,6 +3,6 @@ package com.linecorp.example.testing.db;
 public interface DbContract {
     public static final String HOST = "jdbc:postgres://bnujrtlweyppfu:_sbmES7FJoG1X4RISbcetjQX8K@ec2-54-235-65-139.compute-1.amazonaws.com:5432/db6co2hp5r1sdg";
     public static final String DB_NAME = "frozendb";
-    public static final String USERNAME = "line";
-    public static final String PASSWORD = "line";
+    public static final String USERNAME = "bnujrtlweyppfu";
+    public static final String PASSWORD = "_sbmES7FJoG1X4RISbcetjQX8K";
 }
